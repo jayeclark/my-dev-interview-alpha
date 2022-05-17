4f9fd45e57c00dcd2ebc56c9f1df53184c8c26ca
Jay Clark <jay@jayeclark.dev>
2022-05-17 17:19:06 -0400

    Update config
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_ID,
     AWS_SECRET_ACESS_KEY: process.env.AWS_SECRET_KEY,
     AWS_REGION: process.env.AWS_REGION,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    API_URL: process.env.API_URL,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
+    GOOGLE_AUTH_URI: process.env.GOOGLE_AUTH_URI,
+    REACT_APP_VIDEO_BUCKET: process.env.REACT_APP_VIDEO_BUCKET,
+    REACT_APP_UTIL_REGION: process.env.REACT_APP_UTIL_REGION,
   },
   images: {
     domains: ['fakeface.rest']
