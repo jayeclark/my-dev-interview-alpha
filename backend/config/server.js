17478d46652e46215dffa28e403f0ed533ad20d7
Jay Clark <jay@jayeclark.dev>
2022-05-24 13:23:13 -0400

    herooooookuuuuuuu why
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,12 +1,9 @@
 const vars = process.env;
-let PORT = 1337;
-let URL = 'http://localhost:1337'
-if (vars.PORT) { PORT = vars.PORT }
 if (vars.PUBLIC_URL) { URL = vars.PUBLIC_URL}
 console.log(PORT)
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', PORT),
+  port: env.int('PORT', 1337),
   url: env('', URL),
   app: {
     keys: env.array('APP_KEYS'),
