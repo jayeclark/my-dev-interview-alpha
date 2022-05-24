077fdae421dfa8a1eda30b603ebc986e11df5257
Jay Clark <jay@jayeclark.dev>
2022-05-24 12:18:46 -0400

    Fix url
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,15 @@
+const vars = process.env;
+let HOST = '0.0.0.0';
+let PORT = 1337;
+let URL = 'http://localhost:1337';
+if (vars.PUBLIC_URL) { URL = vars.PUBLIC_URL }
+if (vars.PORT) { PORT = vars.PORT }
+if (vars.HOST) { HOST = vars.HOST }
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('', 'http://localhost:1337'),
+  host: env('HOST', HOST),
+  port: env.int('PORT', PORT),
+  url: env('URL', URL),
   app: {
     keys: env.array('APP_KEYS'),
   },
