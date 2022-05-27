fc28836a0f9b0615694cbc5aa38d95be87503a70
Jay Clark <jay@jayeclark.dev>
2022-05-27 17:18:10 -0400

    Prettier landing page
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,68 +1,137 @@
-import { useState } from "react" 
-import Image from "next/image"
-import { useRouter } from "next/router"
-import { Button, useTheme } from "@mui/material"
-import hero from "../assets/interview.jpg"
-import arrow from "../assets/arrow-right-short.svg"
-import video from "../assets/video.png"
-import SignInForm from "./SignInForm"
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Button, useTheme } from "@mui/material";
+import hero from "../assets/interview.jpg";
+import arrow from "../assets/arrow-right-short.svg";
+import video from "../assets/video.png";
+import SignInForm from "./SignInForm";
 
-const plan = (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 16 16">
-  <path fillRule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
-</svg>)
+const plan = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fillRule="evenodd"
+      d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"
+    />
+  </svg>
+);
 
-const record = (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 16 16">
-  <path fillRule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"/>
-</svg>)
+const record = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path
+      fillRule="evenodd"
+      d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"
+    />
+  </svg>
+);
 
-const share = (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="0 0 16 16">
-  <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z"/>
-</svg>)
+const share = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100%"
+    height="100%"
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
+  </svg>
+);
 
 export default function LandingPage() {
   const theme = useTheme();
   const router = useRouter();
-  
+
   const [showSignIn, setShowSignIn] = useState(false);
   const handleShowSignin = (bool: boolean) => {
     setShowSignIn(bool);
-  }
+  };
 
   return (
     <>
       <div className="hero">
         <div className="hero-img">
-          <Image width="1100" height="600" layout="responsive" src={hero} alt="video interview" />
+          <Image
+            width="1100"
+            height="600"
+            layout="responsive"
+            src={hero}
+            alt="video interview"
+          />
         </div>
-        <div className="landing">
-          
-        </div>
+        <div className="landing"></div>
         <div className="cta">
           <h1>Ace the virtual interview</h1>
-          <p className="cta-details">Plan, practice and get feedback on your answers to video interview questions.</p>
+          <p className="cta-details">
+            Plan, practice and get feedback on your answers to video interview
+            questions.
+          </p>
           <div>
-            <Button variant="contained" color="info" sx={{ mr: 2, borderRadius: "50px" }} onClick={() => setShowSignIn(true)}>Get Started</Button><Button onClick={() => router.push("/practice")} variant="outlined" color="info" sx={{  borderRadius: "50px" }}>Try it Out</Button>
+            <Button
+              size="large"
+              variant="contained"
+              color="info"
+              sx={{ mr: 2, borderRadius: "50px" }}
+              onClick={() => setShowSignIn(true)}
+            >
+              Get Started
+            </Button>
+            <Button
+              size="large"
+              onClick={() => router.push("/practice")}
+              variant="outlined"
+              color="info"
+              sx={{ borderRadius: "50px" }}
+            >
+              Try it Out
+            </Button>
           </div>
         </div>
       </div>
       <div>
-        <h1 className="steps-title">How <span className="desktop">MyDevInterview</span><span className="mobile">it</span> Works</h1>
+        <h1 className="steps-title">
+          How <span className="desktop">MyDevInterview</span>
+          <span className="mobile">it</span> Works
+        </h1>
         <div className="steps">
           <div className="step">
             <div className="icon">{plan}</div>
             <h3>Plan answers</h3>
           </div>
           <div className="arrow">
-            <Image src={arrow} width={40} height={40} alt="right arrow"/>
-            <h3>&nbsp;<span className="mobile"><br/>&nbsp;</span></h3>
+            <Image src={arrow} width={40} height={40} alt="right arrow" />
+            <h3>
+              &nbsp;
+              <span className="mobile">
+                <br />
+                &nbsp;
+              </span>
+            </h3>
           </div>
           <div className="step">
             <div className="icon">{record}</div>
             <h3>Record videos</h3>
           </div>
           <div className="arrow">
-            <Image src={arrow} width={40} height={40} alt="right arrow"/>
-            <h3>&nbsp;<span className="mobile"><br/>&nbsp;</span></h3>
+            <Image src={arrow} width={40} height={40} alt="right arrow" />
+            <h3>
+              &nbsp;
+              <span className="mobile">
+                <br />
+                &nbsp;
+              </span>
+            </h3>
           </div>
           <div className="step">
             <div className="icon">{share}</div>
@@ -70,13 +139,26 @@ export default function LandingPage() {
           </div>
         </div>
         <div className="steps-cta">
-          <Button onClick={() => setShowSignIn(true)} size="large" sx={{ width: "30vw", minWidth: "300px", borderRadius: "50px", mr: 2 }} variant="contained" color="primary" >Get Started</Button>
+          <Button
+            onClick={() => setShowSignIn(true)}
+            size="large"
+            sx={{
+              width: "30vw",
+              minWidth: "300px",
+              borderRadius: "50px",
+              mr: 2,
+            }}
+            variant="contained"
+            color="primary"
+          >
+            Get Started
+          </Button>
         </div>
       </div>
       <div className="features">
         <div>
           <Image src={video} alt="video call" width={540} height={360} />
-        </div> 
+        </div>
         <div className="features-text">
           <h2>Features</h2>
           <div className="features-list">
@@ -85,13 +167,20 @@ export default function LandingPage() {
               <li>Plan STAR, CARL &amp; SOAR responses</li>
               <li>Practice, rate and save video responses</li>
               <li>Share responses with friends and mentors</li>
-              <li><span className="desktop">Use social feedback to</span><span className="mobile">Get feedback &amp;</span> improve your performance</li>
+              <li>
+                <span className="desktop">Use social feedback to</span>
+                <span className="mobile">Get feedback &amp;</span> improve your
+                performance
+              </li>
             </ul>
           </div>
-          
         </div>
       </div>
-      <SignInForm signUpMode={true} showSignIn={showSignIn} setShowSignIn={handleShowSignin} />
+      <SignInForm
+        signUpMode={true}
+        showSignIn={showSignIn}
+        setShowSignIn={handleShowSignin}
+      />
       <style jsx>{`
       .icon {
         box-shadow: 1px 1px 4px rgba(0,0,0,0.1);
@@ -297,8 +386,11 @@ export default function LandingPage() {
           font-weight: 300;
           font-size: 0.9remrem;
         }
+        @media only screen and (min-width: 1200px) {
+
+        }
       }
       `}</style>
     </>
-  )
+  );
 }
