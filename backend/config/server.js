92f41149c157e5e3c37896a36f9fa3932c823ac3
Jay Clark <jay@jayeclark.dev>
2023-01-27 12:00:32 -0800

    Update port logic
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,13 @@
 const vars = process.env;
 let URL = 'http://localhost:1337'
-if (vars.PUBLIC_URL) { URL = vars.PUBLIC_URL}
+let PORT = 1337
+if (vars.PUBLIC_URL) {
+  URL = vars.PUBLIC_URL
+  PORT = vars.PORT
+}
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', PORT),
   url: env('PUBLIC_URL', 'https://backend-sheltered-shelf-66946.herokuapp.com/'),
   app: {
     keys: env.array('APP_KEYS'),
